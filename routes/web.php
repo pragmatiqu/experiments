11d16cd708e15f03136d28415c66a3c0fcd85cbb
Michael Gerzabek <michael.gerzabek@gmail.com>
2025-03-11 10:07:39 +0100

    Experiment 3 erfolgreich abgeschlossen

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/ui5/hybrid', function () {
+    return view('ui5.hybrid');
+});
+
+Route::get('/ui5/index', function () {
+    return view('ui5.index');
+});
+Route::get('/ui5/manifest.json', function () {
+    return response()->json(
+        json_decode(
+            view('ui5.manifest', [
+                'appTitle' => 'Hello World!',
+                'appDescription' => 'This is app description.',
+            ])->render(),
+            true
+        )
+    );
+});
+Route::get('/ui5/Component-preload.js', function () {
+    return response()->file(resource_path('views/ui5/Component-preload.js'), [
+        'Content-Type' => 'application/javascript'
+    ]);
+});
+Route::get('/ui5/Component-dbg.js', function () {
+    return response()->file(resource_path('views/ui5/Component-dbg.js'), [
+        'Content-Type' => 'application/javascript'
+    ]);
+});
+Route::get('/ui5/{string}.js.map', function (string $string) {
+    return response()->file(resource_path("views/ui5/portal/{$string}.js.map"), [
+        'Content-Type' => 'application/json'
+    ]);
+});
+Route::get('/ui5/css/style.css', function () {
+    return response()->file(resource_path('views/ui5/style.css'), [
+        'Content-Type' => 'text/css'
+    ]);
+});
 
 Route::middleware([
     'auth:sanctum',
